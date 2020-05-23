54d19c1e3c4f1282ac426117f4cac2cae7cb77d0
ElderSam <eldersamuel98@gmail.com>
2020-05-22 21:02:25 -0300

    criando a primeira rota

--- src/server.js
+++ src/server.js
@@ -11,6 +11,14 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, 'files')));
 app.use(routes);
 
+app.get('/', (request, response) => {
+  return response.send(
+    {
+      msg : 'Hello World!',
+      name: 'Backend do App Consultaí'
+    }
+  );
+});
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Backend Consultai is listening!');
